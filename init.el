78f3719e70fe93b01eb335791af533fbe8c2d767
D Smith <daniels@giveyourmilk.org>
2021-06-04 16:04:38 -0600

    Roam Server

--- init.el
+++ init.el
@@ -71,6 +71,20 @@
 (add-hook 'org-roam-mode-hook #'org-roam-bibtex-mode)
 (straight-use-package 'org-download)
 (add-hook 'dired-mode-hook 'org-download-enable)
+(straight-use-package 'org-roam-server)
+(setq org-roam-server-host "127.0.0.1"
+	org-roam-server-port 8080
+	org-roam-server-authenticate nil
+	org-roam-server-export-inline-images t
+	org-roam-server-serve-files nil
+	org-roam-server-served-file-extensions '("pdf" "mp4" "ogv")
+	org-roam-server-network-poll t
+	org-roam-server-network-arrows nil
+	org-roam-server-network-label-truncate t
+	org-roam-server-network-label-truncate-length 60
+	org-roam-server-network-label-wrap-length 20)
+
+(org-roam-server-mode)
 
 (menu-bar-mode -1)
 (tool-bar-mode -1) 
